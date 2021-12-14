0c68fffe84829818903e56c37124e4a6c78ee33f
Ohle Claussen <claussen@jpk.com>
2021-12-14 13:12:39 +0100

    Attempt to find text

--- src/main/java/de/eudaemon/swag/ComponentDescription.java
+++ src/main/java/de/eudaemon/swag/ComponentDescription.java
@@ -1,5 +1,8 @@
 package de.eudaemon.swag;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import java.io.Serializable;
 
 import java.awt.Component;
@@ -11,16 +14,33 @@ public class ComponentDescription implements Serializable {
     public final String simpleClassName;
     public final String name;
 
-    public ComponentDescription(String className_, String simpleClassName_, String name_) {
+    public final String text;
+
+    public ComponentDescription(
+            String className_, String simpleClassName_, String name_, String text_) {
         className = className_;
         simpleClassName = simpleClassName_;
         name = name_;
+        text = text_;
     }
 
     public static ComponentDescription forComponent(Component component) {
         return new ComponentDescription(
                 component.getClass().getName(),
                 component.getClass().getSimpleName(),
-                component.getName());
+                component.getName(),
+                findTextIfAny(component));
+    }
+
+    private static String findTextIfAny(Component component) {
+        // There's no generic interface for a component that has a displayed text, but a lot
+        // of component subtypes have a getText() method; so find and use it reflectively.
+        try {
+            Method getText = component.getClass().getMethod("getText");
+            Object returnValue = getText.invoke(component);
+            return (String) returnValue;
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e_) {
+            return null;
+        }
     }
 }
